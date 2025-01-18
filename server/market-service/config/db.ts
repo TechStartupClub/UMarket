f90bcd34c482387505fe9adb1be88c270fa110d8
njordan <njordan1@uw.edu>
2025-01-18 12:05:54 -0800

    Chore: Setup database connections with all microservices and set file structure

---
+++ server/market-service/config/db.ts
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const marketPool = new Pool({
+    user: process.env.PGUSER,   
+    host: process.env.PGHOST || 'localhost', 
+    database: process.env.PGNAME,   
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT || 5432,
+});
+
+module.exports = {
+    query: (text: string, params: any) => marketPool.query(text, params),
+}
