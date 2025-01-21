ca29cd4e2ece07e9be1fc295351cfecb9996db25
njordan <njordan1@uw.edu>
2025-01-20 18:07:54 -0800

    Update changes requested in PR, Add graceful shut down to Pool Connection on app termination or interuption.

--- server/market-service/config/db.ts
+++ server/market-service/config/db.ts
@@ -3,11 +3,14 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const marketPool = new Pool({
-    user: process.env.PGUSER,   
-    host: process.env.PGHOST || 'localhost', 
-    database: process.env.PGNAME,   
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT || 5432,
+    user: process.env.PG_USER,   
+    host: process.env.PG_HOST, 
+    database: process.env.PG_NAME,   
+    password: process.env.PG_SERVICE_ROLE_KEY,
+    port: process.env.PG_PORT || 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 module.exports = {
