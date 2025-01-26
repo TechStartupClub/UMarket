1f11e01de25029850ac314d63fd678b165412c64
celestinryf <celestinryf@gmail.com>
2025-01-26 12:41:16 -0800

    feat: topbar route functionality

--- client/src/shared/components/layout/Topbar/index.tsx
+++ client/src/shared/components/layout/Topbar/index.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Topbar.module.css';
+import { HiShoppingCart } from 'react-icons/hi';
 
 const Topbar: React.FC = () => {
-    return (
-        <div className={styles.topbar}>
-            <div className={styles.content}>
-                <div className={styles.left}>
-                <span>Hello {'<insert username>'}! </span>
-                <span>Help & Contact</span>
-                <span>About</span>
-                </div>
-                <div className={styles.right}>
-                    <span>Sell</span>
-                    <span>Watchlist</span>
-                    <span>My Profile</span>
-                    <span>Inbox</span>
-                    <span>Cart 🛒</span>
-                </div>
-            </div>
-        </div>
-    );
+   return (
+       <div className={styles.topbar}>
+           <div className={styles.content}>
+               <div className={styles.left}>
+                   <Link to="/profile"><span>Hello {'<insert username>'}!</span></Link>
+                   <Link to="/help"><span>Help & Contact</span></Link>
+                   <Link to="/about"><span>About</span></Link>
+               </div>
+               <div className={styles.right}>
+                   <Link to="/sell"><span>Sell</span></Link>
+                   <Link to="/watchlist"><span>Watchlist</span></Link>
+                   <Link to="/profile"><span>My Profile</span></Link>
+                   <Link to="/inbox"><span>Inbox</span></Link>
+                   <Link to="/cart"><span>Cart <HiShoppingCart size={16} /></span></Link>
+               </div>
+           </div>
+       </div>
+   );
 };
 
 export default Topbar;
