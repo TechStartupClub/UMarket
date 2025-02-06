f8023154158bc555c892ac05777aaa08030abf1f
Anthony <antnay@uw.edu>
2025-02-06 01:09:32 -0800

    update: added future sql query

--- server/social-service/src/controllers/socialControllers.ts
+++ server/social-service/src/controllers/socialControllers.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
-
-const db = require("../config/db");
+import socialPool from "../config/db";
 
 export const userPosts = async (req: Request, res: Response): Promise<void> => {
     console.log(req.params);
-    res.status(200).json({
-        message: "User posts",
-    })
+    try {
+        // const result = await socialPool.query(
+        //     'SELECT * FROM posts WHERE userId = $1', [req.params]
+        // )
+        res.status(200).json({
+            message: "replace with result"
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Server error" });
+    }
 };
 
 export const postComments = async (req: Request, res: Response): Promise<void> => {
