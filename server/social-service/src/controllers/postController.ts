9ad85771aa8eb709e776e36cd93271b21792724f
Anthony <antnay@uw.edu>
2025-02-10 20:49:21 -0800

    fix: correctly parse user id

--- server/social-service/src/controllers/postController.ts
+++ server/social-service/src/controllers/postController.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 import socialPool from "../config/db";
 
 export const userPosts = async (req: Request, res: Response): Promise<void> => {
-    console.log(req.params);
     try {
+        const userId: number = parseInt(req.params.userId, 10);
+        if (isNaN(userId)) {
+            res.status(400).send({ error: "Invalid user id"});
+            return;
+        }
         const result = await socialPool.query(
-            'SELECT * FROM posts WHERE userId = $1', [req.params]
+            'SELECT * FROM posts WHERE user_id = $1', [userId]
         )
-        // console.log(result)
         res.status(200).send(result.rows)
     } catch (error) {
         console.log(error)
