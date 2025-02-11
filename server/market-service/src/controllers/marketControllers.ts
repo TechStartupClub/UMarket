04dce155287a4085cd6de5d80f1711997754d640
James <jamesl.escudero@gmail.com>
2025-02-10 19:25:09 -0800

    fix: get user item route and controller

--- server/market-service/src/controllers/marketControllers.ts
+++ server/market-service/src/controllers/marketControllers.ts
@@ -1,16 +1,32 @@
+import { Request, Response} from 'express';
 import marketPool from '../config/db';
 
-export const getRecentItems = async (req: any, res: any) => {
-    const { amount = 10, page = 1 } = req.query; // Default values
+export const getRecentItems = async (req: Request, res: Response) => {
+        try {
+        // Checking the amount query parameter to see how many items the client wants
+        // If nothing, default to 10
+        const amount = parseInt(req.query.amount as string, 10) || 10;
+        
+        // If the amount is greater than 50 or less than 0, return an error
+        if(amount <= 0 || amount > 100) {
+            return res.status(400).json({ error: 'Invalid amount' });
+        }
 
-    try {
+        // Querying the database for the amount of items the client wants
         const result = await marketPool.query(
-            'SELECT itemId, itemName, price, image FROM items ORDER',
+            'SELECT item_id, name, price, media_url FROM items LIMIT $1',
             [amount]
-        );
+        )
 
+        if(result.rows.length === 0) {
+            return res.status(404).json({ error: 'No items found' });
+        }
+        
+        // Return the items to the client
+        return res.status(200).json(result.rows);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Server Error');
+        // Sending back the erorr message to the client
+        res.status(500).json( {error: "Server Error" });
     }
 };
--- server/market-service/src/routes/marketRoutes.ts
+++ server/market-service/src/routes/marketRoutes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+// importing the getRecentItems function from marketControllers
 import { getRecentItems } from '../controllers/marketControllers';
 
+// creating a new router object
 const router = express.Router();
 
-router.get('/items/recent', getRecentItems);
+// '/items/recent' is the endpoint for this route
+// getRecentItems is the controller function that will be called when the endpoint is hit
+router.get("/market/items/recent", getRecentItems);
 
+// then export
 export default router;
