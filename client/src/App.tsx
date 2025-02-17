c34b320569ced05353ed824fe293849ef0261497
Austin Nguyen <austin11@uw.edu>
2025-02-17 14:52:30 -0800

    Feat: About Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PageLayout from './shared/components/layout/PageLayout';
 import HomePage from './shared/routes/Home';
-
+import AboutPage from './shared/routes/About';
 const App: React.FC = () => {
    return (
        <Router>
            <PageLayout>
                <Routes>
                    <Route path="/" element={<HomePage />} />
+                   <Route path="/about" element={<AboutPage />} />
                </Routes>
            </PageLayout>
        </Router>
