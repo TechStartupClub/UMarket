705d9466158561619b1b7ad2f4c0bc2ceb11031a
Anthony <antnay@uw.edu>
2025-02-18 21:03:50 -0800

    feat: create social post route

--- server/social-service/src/routes/socialRoutes.ts
+++ server/social-service/src/routes/socialRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { userPosts } from "../controllers/postController";
-import {postComments} from "../controllers/commentController";
+import { createPost, updatePost, userPosts } from "../controllers/postController";
+import { postComments } from "../controllers/commentController";
 
 const router = Router();
 
 router.get("/user/:userId/posts", userPosts);
 router.get("/comments/post/:postId", postComments)
+router.post("/posts/create", createPost);
+router.patch("/posts/update/:postId", updatePost);
 
 export default router;
