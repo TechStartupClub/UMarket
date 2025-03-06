2d6eb7e85b56dee0187cc9258f5077e2e9422e11
Austin Nguyen <austin11@uw.edu>
2025-03-06 01:26:22 -0800

    Refactor: renaming function for clarify

--- client/src/market/routes/Item Listing/index.tsx
+++ client/src/market/routes/Item Listing/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import style from './index.module.css'
 
 function index() {
   return (
-    <div>index</div>
+    <div className={style.itemListingWrapper}>
+      <div className={style.innerListingWrapper}></div>
+    </div>
   )
 }
 
