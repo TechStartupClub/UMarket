10146db5b8b95f8b16b0142700d1446714228830
Anthony <antnay@uw.edu>
2025-04-20 17:15:10 -0700

    fix: add back getUserItems route which was deleted

--- server/market-service/src/routes/marketRoutes.ts
+++ server/market-service/src/routes/marketRoutes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getRecentItems, getUserItems } from '../controllers/marketControllers'; // Import the controller function
+import { getRecentItems, getUserItems, createItem, deleteItem } from '../controllers/marketControllers';
 
 // Create a new router
 const router = express.Router();
 
 // recent amount is the query parameter that will be passed to the controller
 router.get("/items/recent", getRecentItems);
+router.get("/items/create", createItem)
+router.get("/items/:itemId/delete", deleteItem)
 router.get("/items/user/:userId", getUserItems)
 
 export default router;
