bc233b2e8b87b8c877289a5babe521eb472c88eb
Matthew Franchini <matchewf11@gmail.com>
2025-05-13 17:00:48 -0700

    Update post implemented, null in json to avoid changing the value

--- server/social-service/src/controllers/protectedPostCont.ts
+++ server/social-service/src/controllers/protectedPostCont.ts
@@ -35,11 +35,68 @@ export const createPost = async (
   }
 };
 
-export const updatePost = async (
-  req: Request,
-  res: Response,
-): Promise<void> => {
-  res.status(200).json({ message: "Success" });
+export const updatePost = async (req: Request, res: Response): Promise<void> => {
+  try {
+    
+    const post_id: number = parseInt(req.params.postId, 10)
+
+    if (isNaN(post_id)) {
+      res.status(400).send({ error: "Invalid post id" })
+      return;
+    }
+
+    const parsedData = await createPostSchema.parse(req.body);
+    const { text_content, media_type, media_url } = parsedData;
+
+    // enusure that posts exist
+    const postExists = await socialPool.query(
+      `
+      SELECT 1 FROM posts WHERE post_id = ($1)
+      `, [post_id]
+    )
+    if (postExists.rowCount === 0) {
+      res.status(400).send({ error: "Post doesn't exist" })
+      return;
+    }
+
+    if (text_content !== "null") {
+      await socialPool.query(
+        `
+        UPDATE posts SET text_content = ($1) WHERE post_id = ($2)
+        `, [text_content, post_id]
+      );
+    }
+
+    if (media_type !== "null") {
+      await socialPool.query(
+        `
+        UPDATE posts SET media_type = ($1) WHERE post_id = ($2)
+        `, [media_type, post_id]
+      );
+    }
+
+    if (media_url !== "null") {
+      await socialPool.query(
+        `
+        UPDATE posts SET media_url = ($1) WHERE post_id = ($2)
+        `, [media_url, post_id]
+      );
+    }
+
+    // get the post and return the new one
+    const result = await socialPool.query(
+      `
+      SELECT * FROM posts WHERE post_id = ($1)
+      `, [post_id]
+    );
+
+    res.status(200).send({ post: result.rows })
+
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" })
+  }
+
 };
 
 export const likePost = async (req: Request, res: Response): Promise<void> => {
@@ -70,7 +127,7 @@ export const likePost = async (req: Request, res: Response): Promise<void> => {
             INSERT INTO post_likes (post_id, user_id)
             VALUES ($1, $2)
         `,
-      [post_id, user_id],
+      [post_id, user_id]
     );
 
     const result = await socialPool.query(
@@ -103,7 +160,7 @@ export const unlikePost = async (req: Request, res: Response): Promise<void> => 
     const alreadyContains = await socialPool.query(
       `
       SELECT 1 FROM post_likes WHERE user_id = ($1) AND post_id = ($2)  
-      `, [user_id, post_id],
+      `, [user_id, post_id]
     );
 
     if (alreadyContains.rowCount === 0) {
@@ -114,14 +171,14 @@ export const unlikePost = async (req: Request, res: Response): Promise<void> => 
     await socialPool.query(
       `
      DELETE FROM post_likes WHERE user_id = ($1) AND post_id = ($2)
-      `, [user_id, post_id],
+      `, [user_id, post_id]
     );
 
     const result = await socialPool.query(
       `
             SELECT post_id::bigint AS estimate FROM post_likes where post_id =  $1;
         `,
-      [post_id],
+      [post_id]
     );
     res.status(200).send({ likeCount: result.rowCount });
   } catch (error) {
